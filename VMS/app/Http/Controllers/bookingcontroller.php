1a47b80b937c9d6a57c5f6aeaec46dd89eb3038d
nivas <nivasram484@gmail.com>
2017-03-26 22:54:15 +0530

    Update-26-03-17-8PM

--- VMS/app/Http/Controllers/bookingcontroller.php
+++ VMS/app/Http/Controllers/bookingcontroller.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\bookingmodel;
 use Redirect;
 use DB;
+use Mail;
 
 class bookingcontroller extends Controller
 {
@@ -39,26 +40,98 @@ class bookingcontroller extends Controller
 
     }
     public function rejectofficialvisitupdate(Request $request)
-    {  $employeeinfo=$request->input('employeeinfo');
+    {   $employeeinfo=$request->input('employeeinfo');
        $id=$request->input('id');
+       $visitoremail=$request->input('visitoremail');
+       $visitorname=$request->input('visitorname');
+       $visitorphonenumber=$request->input('visitorphonenumber');
+       $visitortype=$request->input('visitortype');
+       $empname=$request->input('empname');
+       $empdept=$request->input('empdept');
+       $date=$request->input('date');
+       $from=$request->input('from');
+       $noofhours=$request->input('noofhours');
+
+
+       Mail::send('mails.rejectedofficialvisit',['visitorname'=>$visitorname,'visitorphonenumber'=>$visitorphonenumber,
+                                                  'visitoremail'=>$visitoremail,'id'=>$id,'visitortype'=>$visitortype,
+                                                  'empname'=>$empname,'empdept'=>$empdept,'date'=>$date,'from'=>$from,
+                                                  'noofhours'=>$noofhours,'employeeinfo'=>$employeeinfo],function($message) use($visitoremail, $visitorname)
+       {
+         $message->to($visitoremail,$visitorname)->subject('Official Visit Request Rejected');
+       });
        DB::update('update bookingtable set employeeinfo=?,staus="Rejected" where id=?',[$employeeinfo,$id]);
        return Redirect::to('employee_ovreq')->with('success','Successfully Rejected Official Visit Request!!!');
     }
     public function rejectpersonalvisitupdate(Request $request)
-    {  $employeeinfo=$request->input('employeeinfo');
+    {   $employeeinfo=$request->input('employeeinfo');
        $id=$request->input('id');
+       $visitoremail=$request->input('visitoremail');
+       $visitorname=$request->input('visitorname');
+       $visitorphonenumber=$request->input('visitorphonenumber');
+       $visitortype=$request->input('visitortype');
+       $empname=$request->input('empname');
+       $empdept=$request->input('empdept');
+       $date=$request->input('date');
+       $from=$request->input('from');
+       $noofhours=$request->input('noofhours');
+
+
+       Mail::send('mails.rejectedpersonalvisit',['visitorname'=>$visitorname,'visitorphonenumber'=>$visitorphonenumber,
+                                                  'visitoremail'=>$visitoremail,'id'=>$id,'visitortype'=>$visitortype,
+                                                  'empname'=>$empname,'empdept'=>$empdept,'date'=>$date,'from'=>$from,
+                                                  'noofhours'=>$noofhours,'employeeinfo'=>$employeeinfo],function($message) use($visitoremail, $visitorname)
+       {
+         $message->to($visitoremail,$visitorname)->subject('Personal Visit Request Rejected');
+       });
        DB::update('update bookingtable set employeeinfo=?,staus="Rejected" where id=?',[$employeeinfo,$id]);
        return Redirect::to('employee_pvreq')->with('success','Successfully Rejected Personal Visit Request!!!');
     }
     public function acceptofficialvisitupdate(Request $request)
-    {  $employeeinfo=$request->input('employeeinfo');
+    {   $employeeinfo=$request->input('employeeinfo');
        $id=$request->input('id');
+       $visitoremail=$request->input('visitoremail');
+       $visitorname=$request->input('visitorname');
+       $visitorphonenumber=$request->input('visitorphonenumber');
+       $visitortype=$request->input('visitortype');
+       $empname=$request->input('empname');
+       $empdept=$request->input('empdept');
+       $date=$request->input('date');
+       $from=$request->input('from');
+       $noofhours=$request->input('noofhours');
+
+
+       Mail::send('mails.acceptedofficialvisit',['id'=>$id,'visitorname'=>$visitorname,'visitorphonenumber'=>$visitorphonenumber,
+                                                  'visitoremail'=>$visitoremail,'id'=>$id,'visitortype'=>$visitortype,
+                                                  'empname'=>$empname,'empdept'=>$empdept,'date'=>$date,'from'=>$from,
+                                                  'noofhours'=>$noofhours,'employeeinfo'=>$employeeinfo],function($message) use($visitoremail, $visitorname)
+       {
+         $message->to($visitoremail,$visitorname)->subject('Official Visit Request Accepted');
+       });
        DB::update('update bookingtable set employeeinfo=?,staus="Approved" where id=?',[$employeeinfo,$id]);
        return Redirect::to('acceptedofficialvisits')->with('success','Successfully Accepted Official Book Request!!!');
     }
     public function acceptpersonalvisitupdate(Request $request)
     {  $employeeinfo=$request->input('employeeinfo');
        $id=$request->input('id');
+       $visitoremail=$request->input('visitoremail');
+       $visitorname=$request->input('visitorname');
+       $visitorphonenumber=$request->input('visitorphonenumber');
+       $visitortype=$request->input('visitortype');
+       $empname=$request->input('empname');
+       $empdept=$request->input('empdept');
+       $date=$request->input('date');
+       $from=$request->input('from');
+       $noofhours=$request->input('noofhours');
+
+
+       Mail::send('mails.acceptedpersonalvisit',['id'=>$id,'visitorname'=>$visitorname,'visitorphonenumber'=>$visitorphonenumber,
+                                                  'visitoremail'=>$visitoremail,'id'=>$id,'visitortype'=>$visitortype,
+                                                  'empname'=>$empname,'empdept'=>$empdept,'date'=>$date,'from'=>$from,
+                                                  'noofhours'=>$noofhours,'employeeinfo'=>$employeeinfo],function($message) use($visitoremail, $visitorname)
+       {
+         $message->to($visitoremail,$visitorname)->subject('Personal Visit Request Accepted');
+       });
        DB::update('update bookingtable set employeeinfo=?,staus="Approved" where id=?',[$employeeinfo,$id]);
        return Redirect::to('acceptedpersonalvisits')->with('success','Successfully Accepted Personal Book Request!!!');
     }
---
+++ VMS/resources/views/mails/rejectedofficialvisit.blade.php
@@ -0,0 +1,23 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Booking Response</title>
+</head>
+<body>
+    <h1><center>Visitor Management System</center></h1>
+    <p>
+
+      Hello {{ $visitorname }},<br/>
+      We are here to inform that your book request to Officially visit {{ $empname }} of {{ $empdept }}
+       on {{ $date }} for {{ $noofhours }} at {{ $from}} has been rejected by {{ $empname }}.
+       For Other info Provided by employee Log in to <a href="http://localhost:8000/">Visitor Management System</a>.
+       This is a System Generated mail,so please dont respond.
+       <strong>YOU ARE STRICTLY INFORMED TO BRING A PRINTOUT OF THIS BOOKING CONFIRMATION.</strong><br/>
+      Thanking You,<br/>
+      Visitor Management System
+    </p>
+</body>
+</html>
